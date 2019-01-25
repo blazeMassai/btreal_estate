ffaeb8b0579f28c5433397d99c0626dad4bfed98
Mr Mollel <mrrewardmollel@gmail.com>
2019-01-25 19:24:05 +0300

    'remove_email_from_settings.py'

--- btreal_estate/settings.py
+++ btreal_estate/settings.py
@@ -148,11 +148,11 @@ MESSAGE_TAGS = {
 }
 
 #email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mrrewardmollel@gmail.com'
-EMAIL_HOST_PASSWORD = 'koloike2991'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'mrrewardmollel@gmail.com'
+# EMAIL_HOST_PASSWORD = 'koloike2991'
+# EMAIL_USE_TLS = True
 
 
 try:
